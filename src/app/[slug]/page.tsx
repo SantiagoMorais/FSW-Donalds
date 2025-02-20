42b8be0588fcdc33615cfa09c69da33f3bfd4a93
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-20 20:55:21 -0300

    feat(data): created function to fetch restaurant details by slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,18 @@
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
 interface IRestaurantPageProps {
   params: Promise<{ slug: string }>;
 }
 
 const RestaurantPage = async ({ params }: IRestaurantPageProps) => {
   const { slug } = await params;
-  return <h1>{slug}</h1>;
+  const restaurant = await getRestaurantBySlug(slug);
+
+  return (
+    <div>
+      <h1>{restaurant?.name}</h1>;
+    </div>
+  );
 };
 
 export default RestaurantPage;

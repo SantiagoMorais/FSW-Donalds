2bed410f308ba852fd34a5464756bbf4b797f30a
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-21 10:03:51 -0300

    feat(ui): add ScrollArea component and integrate it into RestaurantCategories

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -2,6 +2,8 @@ import { Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
 
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
 interface IRestaurantCategoriesProps {
   restaurant: Prisma.RestaurantGetPayload<{
     include: {
@@ -14,28 +16,33 @@ interface IRestaurantCategoriesProps {
   }>;
 }
 
-const RestaurantCategories = ({ restaurant }: IRestaurantCategoriesProps) => {
-  
-  return(
-    <section className="relative z-10 -mt-6 space-y-3 rounded-t-3xl border bg-white p-5">
-      <header className="flex items-center gap-3">
-        <Image
-          src={restaurant.avatarImageUrl}
-          alt={restaurant.name}
-          height={45}
-          width={45}
-        />
-        <div>
-          <h2 className="text-lg font-semibold">{restaurant.name}</h2>
-          <p className="text-sm opacity-55">{restaurant.description}</p>
-        </div>
-      </header>
-      <article className="flex items-center gap-1 text-xs text-green-500">
-        <ClockIcon size={12} />
-        <p>Aberto</p>
-      </article>
-    </section>
-  );
-};
-
-export default RestaurantCategories;
+export const RestaurantCategories = ({
+  restaurant,
+}: IRestaurantCategoriesProps) => (
+  <section className="relative z-10 -mt-6 space-y-3 rounded-t-3xl border bg-white p-5">
+    <header className="flex items-center gap-3">
+      <Image
+        src={restaurant.avatarImageUrl}
+        alt={restaurant.name}
+        height={45}
+        width={45}
+      />
+      <div>
+        <h2 className="text-lg font-semibold">{restaurant.name}</h2>
+        <p className="text-sm opacity-55">{restaurant.description}</p>
+      </div>
+    </header>
+    <article className="flex items-center gap-1 text-xs text-green-500">
+      <ClockIcon size={12} />
+      <p>Aberto</p>
+    </article>
+    <ScrollArea className="w-full">
+      <div className="flex w-max space-x-4">
+        {restaurant.menuCategories.map((category) => (
+          <p key={category.id}>{category.name}</p>
+        ))}
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
+  </section>
+);
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 import { isConsumptionMethodValid } from "@/utils/is-consumption-method-valid";
 
-import RestaurantCategories from "./components/categories";
+import { RestaurantCategories } from "./components/categories";
 import { RestaurantHeader } from "./components/header";
 
 interface IRestaurantMenuPageProps {

40dca8fe5fe7fbbcd52061269b78326794b4500b
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-21 09:34:44 -0300

    feat(getRestaurantBySlug): include menuCategories in the function return

--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -3,6 +3,7 @@ import { db } from "@/lib/prisma";
 export const getRestaurantBySlug = async (slug: string) => {
   const restaurant = await db.restaurant.findUnique({
     where: { slug },
+    include: { menuCategories: true },
   });
 
   return restaurant;
